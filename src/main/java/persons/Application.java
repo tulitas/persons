b25f050176ec2842bc5a175e3f6f6c6fe5becca2
sergej <tulitas1@gmail.com>
2020-10-18 09:04:32 +0300

    CSV import fixing

--- src/main/java/persons/Application.java
+++ src/main/java/persons/Application.java
@@ -1,32 +1,22 @@
 package persons;
 
-import org.apache.logging.log4j.jul.LogManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-
-import static com.sun.org.apache.bcel.internal.util.SecuritySupport.getResourceAsStream;
 
 @SpringBootApplication
 @Configuration
 public class Application extends SpringBootServletInitializer {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         SpringApplication.run(Application.class, args);
-//        LogManager.getLogManager().readConfiguration(Application.class.getResourceAsStream("src/main/resources/logging.properties"));
-
     }
-
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
         return applicationBuilder.sources(Application.class);
     }
-
-
 }
